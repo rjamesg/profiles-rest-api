1f1245a738f2eeb5b022b8e547606d698d1e3dba
Ryan Gallagher <ryan.gallagher@live.com.au>
2020-09-24 00:13:14 +1000

    Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,75 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """
+    Manager for user profiles
+    """
+    def create_user(self, email, name, password=None):
+        """
+        Create a new user profile
+        """
+        if not email:
+            raise ValueError("Please Provide an Email Address!")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        ## Saving to the provided databse.
+        ## will need to change if using Mongo.
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """
+        Create and save a new superuser with given details.
+        """
+        user = self.create_user(name, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+
+        return user
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    This is the database model for users in the system.
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True) ## is user activated?
+    is_staff = models.BooleanField(default=False) ## default users are not staff.
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """
+        retrieve full name of user.
+        """
+        return self.name
+
+    def get_short_name(self):
+        """
+        retrieve short name of user.
+        """
+        return self.name
+
+    def __str__(self):
+        """
+        Return string representation of our user
+
+        Note: recommended for all django models to identify
+        the object (in this case, a user).
+        """
+        return self.email
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_(sz7ckx6an6g$4nsu^p0vnvke-s8(x()u4&6ctm=zam7vx%6w'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=True)
 
 ALLOWED_HOSTS = []
 
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
