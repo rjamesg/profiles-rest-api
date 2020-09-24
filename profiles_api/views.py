1f1ad447ab7d75cd551205227ba40a8c4bdf5cc3
Ryan Gallagher <ryan.gallagher@live.com.au>
2020-09-24 21:32:55 +1000

    Added POSt, PUT, PATCH and DELETE methods to APIVIEW

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """
@@ -9,6 +12,9 @@ class HelloApiView(APIView):
     Addign it to this view, then Django handles it
     by calling the relevant function.
     """
+
+    serializer_class = serializers.HelloSerializer
+
 
     def get(self, request, format=None):
         """
@@ -22,3 +28,44 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """
+        Create a hello message with our name
+        """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'Hello {}'.format(name)
+
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """
+        Handle Updating an object
+
+        usually you'd do a PUT to a specific ID.
+        """
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """
+        Handle a partial update of an object
+
+        usually you'd do a PUT to a specific ID.
+        """
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """
+        Delete an object
+
+        usually you'd do a PUT to a specific ID.
+        """
+        return Response({'method': 'DELETE'})
